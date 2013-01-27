2c511eb792ca7e234e93b51bb58516fe8894580c
Prem  Gopinath <hiprem@gmail.com>
2013-01-27 00:09:17 -0600

    DataManager class addition
    Class not yet completed.

---
+++ AccountsTestApp/DataManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountsTestApp
+{
+    public static class DataManager
+    {
+
+        public static List<string> GetAccounts()
+        {
+            List<string> accountList = new List<string>();
+
+
+
+
+            return accountList;
+        }
+    }
+}
